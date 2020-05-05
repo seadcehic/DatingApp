422df3e9b4fd1296cf8dff1cb4ffe33de572938b
seadcehic <sead.cehic@infinitymesh.com>
2020-05-05 14:23:24 +0200

    last section

--- DatinApp.API/Helpers/AutoMapperProfiles.cs
+++ DatinApp.API/Helpers/AutoMapperProfiles.cs
@@ -19,7 +19,7 @@ namespace DatinApp.API.Helpers
                   .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
                 .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
-
+            CreateMap<UserForUpdateDto, User>();
 
         }
     }
